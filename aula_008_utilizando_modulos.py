6e001c9774877958843f6bac1b9292d853e6dc7c
jairobr1986 <jairobr1986@gmail.com>
2023-04-11 11:40:21 -0300

    treinando import de diversas maneiras

---
+++ aula_008_utilizando_modulos.py
@@ -0,0 +1,5 @@
+#Aula 08 Utilizando módulos
+from math import sqrt, floor
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print('A raiz quadradada de {} é igual {:.2f}.'.format(num,floor(raiz)))
