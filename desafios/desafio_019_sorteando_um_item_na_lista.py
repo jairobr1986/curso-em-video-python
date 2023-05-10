01b287a47c76554cd1c0b253c3bb65cec9c04a18
Jairo Oliveira Santos <jairobr1986@gmail.com>
2023-05-10 12:42:56 -0300

    desafio_019_sorteando_um_item_na_lista, usando random

---
+++ desafios/desafio_019_sorteando_um_item_na_lista.py
@@ -0,0 +1,22 @@
+#Exercício Python 19: Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
+"""""
+#Aqui foi usado random completo
+import random
+n1 = str(input('Primeiro nome: '))
+n2 = str(input('Segundo nome: '))
+n3 = str(input('Terceiro nome: '))
+n4 = str(input('Quarto nome: '))
+lista = [n1, n2, n3, n4]
+escolhido = random.choice(lista)
+print(f'O aluno escolhido foi {escolhido}')
+
+"""""
+#Aqui foi usado from random 
+from random import choice
+n1 = str(input('Primeiro nome: '))
+n2 = str(input('Segundo nome: '))
+n3 = str(input('Terceiro nome: '))
+n4 = str(input('Quarto nome: '))
+lista = [n1, n2, n3, n4]
+escolhido = choice(lista)
+print(f'O aluno escolhido foi {escolhido}')
