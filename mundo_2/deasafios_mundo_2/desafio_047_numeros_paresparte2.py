8e3647909936bd5e828c8f07abc6298442d14e63
Jairo Oliveira Santos <jairobr1986@gmail.com>
2025-09-23 21:22:56 -0300

    desafio 047 parte 2

--- mundo_2/deasafios_mundo_2/desafio_047_numeros_paresparte2.py
+++ mundo_2/deasafios_mundo_2/desafio_047_numeros_paresparte2.py
@@ -1,6 +1,9 @@
 # Exercício Python 47: Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
 
-for pares in range(2, 51, 2):
-    # print(".", end=" ")
-    # print(".", end=" ")
-    print(pares, end=" ")
+# for pares in range(2, 51, 2):
+#     # print(".", end=" ")
+#     # print(".", end=" ")
+#     print(pares, end=" ")
+
+for numeros_pares in range(2 ,51 , 2):
+    print(numeros_pares)
---
+++ mundo_2/deasafios_mundo_2/desafio_048_somar_impares_multiplos_3.py
@@ -0,0 +1,1 @@
+# Exercício Python 48: Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que se encontram no intervalo de 1 até 500.
