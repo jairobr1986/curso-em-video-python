1d2238ccb896fff729a61d36e72ef60d04e00108
Jairo Oliveira Santos <jairobr1986@gmail.com>
2025-09-24 07:40:59 -0300

    Corrigindo nomes dos desafios 008 e 009

--- mundo_2/deasafios_mundo_2/desafio_048_somar_impares_multiplos_3.py
+++ mundo_2/deasafios_mundo_2/desafio_048_somar_impares_multiplos_3.py
@@ -1,1 +1,18 @@
-# Exercício Python 48: Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+# Exercício Python 48: Faça um programa que calcule a soma entre todos os números impares que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+
+#separar números impares - feito
+#somar multiplos de 3 
+#intervalo de 1 até 500 - feito
+contador = 0
+contador2 = 0
+somar = 0
+somar2 = 0
+for intervalo in range(1, 501, 2):
+    somar2 += intervalo
+    contador2 += 1
+
+    if intervalo % 3 == 0:
+        contador += 1
+        somar += intervalo
+
+print(f"A soma dos {contador} é valores solicitados são {somar}, e todos os numeros impares somados foram {somar2}, nesses valores {contador2}.do intervalo ")
