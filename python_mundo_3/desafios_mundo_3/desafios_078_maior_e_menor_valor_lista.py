208db87991de882e1a77cbc168f518db82173af1
Jairo Oliveira Santos <jairobr1986@gmail.com>
2026-02-03 22:14:15 -0300

    Aprendendo

--- python_mundo_3/desafios_mundo_3/desafios_078_maior_e_menor_valor_lista.py
+++ python_mundo_3/desafios_mundo_3/desafios_078_maior_e_menor_valor_lista.py
@@ -5,8 +5,7 @@
 # e as suas respectivas posições na lista.
 
 leitor_numeros = list()
-maior = 0
-menor = 0
+
 while True:
     try:
         leitor_numeros = list(int(input('Digite o número desejado: ')) for _ in range(1, 6))
@@ -16,7 +15,15 @@ while True:
 
     if len(leitor_numeros) == 5:
         break
+
 print(leitor_numeros)
+maior = max(leitor_numeros)
+menor = min(leitor_numeros)
+
+for pos, na_lista in enumerate(leitor_numeros):
+    if maior == na_lista:
+        print(f'O maior número é [{maior}], e está na posição [{pos}]')
+
 print()
 print(f'O maior número é {max(leitor_numeros)}')
 print()
